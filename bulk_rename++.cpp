715ccfedf03d0aa7d2c1880732c56aa85a0dc0b9
siyia2 <46220960+siyia2@users.noreply.github.com>
2024-02-10 14:51:30 +0200

    Update bulk_rename++.cpp

--- bulk_rename++.cpp
+++ bulk_rename++.cpp
@@ -47,6 +47,50 @@ void print_help() {
               << "\n";
 }
 
+std::string fupper_ext(const std::string& extension) {
+    std::string result = extension;
+    std::transform(result.begin(), result.end(), result.begin(), ::toupper);
+    return result;
+}
+
+void rename_file_extension(const fs::path& file_path, const std::string& case_input, bool verbose, int& files_count) {
+    std::string extension = file_path.extension().string();
+    std::string new_extension = extension; // Initialize with original extension
+
+    if (case_input == "lower") {
+        std::transform(new_extension.begin(), new_extension.end(), new_extension.begin(), ::tolower);
+    } else if (case_input == "upper") {
+        std::transform(new_extension.begin(), new_extension.end(), new_extension.begin(), ::toupper);
+    } else if (case_input == "reverse") {
+        std::transform(new_extension.begin(), new_extension.end(), new_extension.begin(), [](unsigned char c) {
+            return std::islower(c) ? std::toupper(c) : std::tolower(c);
+        });
+    } else if (case_input == "fupper") {
+        new_extension = fupper_ext(extension);
+    }
+
+    // Skip renaming if the new extension is the same as the old one
+    if (extension != new_extension) {
+        fs::path new_path = file_path;
+        new_path.replace_extension(new_extension);
+
+        try {
+            fs::rename(file_path, new_path);
+            if (verbose) {
+                std::cout << "\033[0m\033[92mRenamed\033[0m file " << file_path.string() << " to " << new_path.string() << std::endl;
+            }
+            ++files_count;
+        } catch (const fs::filesystem_error& e) {
+            std::cerr << "\033[1;91mError\033[0m: " << e.what() << "\n" << std::endl;
+        }
+    } else {
+        if (verbose) {
+            std::cout << "\033[0m\033[93mSkipped\033[0m file " << file_path.string() << " (extension unchanged)" << std::endl;
+        }
+    }
+}
+
+
 std::string fupper(const std::string& word) {
     std::string result = word;
     if (!result.empty()) {
@@ -57,22 +101,30 @@ std::string fupper(const std::string& word) {
 }
 
 
-void rename_item(const fs::path& item_path, const std::string& case_input, bool is_directory, bool verbose, int& files_count, int& dirs_count) {
+void rename_item(const fs::path& item_path, const std::string& case_input, bool is_directory, bool verbose, int& files_count, int& dirs_count, bool rename_only_extension = false) {
+    if (rename_only_extension && !is_directory) {
+        rename_file_extension(item_path, case_input, verbose, files_count);
+        return;
+    }
+
     std::string name = item_path.filename().string();
     std::string new_name = name; // Initialize with original name
 
-    // Apply case transformation if needed
+    // Apply case transformation only to the filename (not the extension) if needed
+    std::string filename = item_path.stem().string();
+    std::string extension = item_path.extension().string();
+
     if (case_input == "lower") {
-        std::transform(new_name.begin(), new_name.end(), new_name.begin(), ::tolower);
+        std::transform(filename.begin(), filename.end(), filename.begin(), ::tolower);
     } else if (case_input == "upper") {
-        std::transform(new_name.begin(), new_name.end(), new_name.begin(), ::toupper);
+        std::transform(filename.begin(), filename.end(), filename.begin(), ::toupper);
     } else if (case_input == "reverse") {
-        std::transform(new_name.begin(), new_name.end(), new_name.begin(), [](unsigned char c) {
+        std::transform(filename.begin(), filename.end(), filename.begin(), [](unsigned char c) {
             return std::islower(c) ? std::toupper(c) : std::tolower(c);
         });
     } else if (case_input == "fupper") {
         bool first_letter_encountered = true;
-        for (char& c : new_name) {
+        for (char& c : filename) {
             if (std::isalpha(c)) {
                 if (first_letter_encountered) {
                     c = std::toupper(c);
@@ -83,6 +135,8 @@ void rename_item(const fs::path& item_path, const std::string& case_input, bool 
             }
         }
     }
+
+    new_name = filename + extension; // Combine the modified filename with the original extension
 
     // Skip renaming if the new name is the same as the old name
     if (name != new_name) {
@@ -109,6 +163,9 @@ void rename_item(const fs::path& item_path, const std::string& case_input, bool 
         }
     }
 }
+
+
+
 
 
 void rename_directory(const fs::path& directory_path, const std::string& case_input, bool rename_immediate_parent, bool verbose, int& files_count, int& dirs_count) {
@@ -182,7 +239,7 @@ void rename_directory(const fs::path& directory_path, const std::string& case_in
 
 
 
-void rename_path(const std::vector<std::string>& paths, const std::string& case_input, bool rename_immediate_parent, bool verbose = true) {
+void rename_path(const std::vector<std::string>& paths, const std::string& case_input, bool rename_immediate_parent, bool verbose = true, bool rename_only_extension = false) {
     // Check if case_input is empty
     if (case_input.empty()) {
         print_error("\033[1;91mError: Case conversion mode not specified (-c option is required)\n\033[0m");
@@ -220,7 +277,11 @@ void rename_path(const std::vector<std::string>& paths, const std::string& case_
                 }
             } else if (fs::is_regular_file(current_path)) {
                 // For files, directly rename the item without considering the parent directory
-                rename_item(current_path, case_input, false, verbose, files_count, dirs_count);
+                if (rename_only_extension) {
+                    rename_file_extension(current_path, case_input, verbose, files_count);
+                } else {
+                    rename_item(current_path, case_input, false, verbose, files_count, dirs_count);
+                }
             } else {
                 print_error("\033[1;91mError: specified path is neither a directory nor a regular file\033[0m\n");
             }
@@ -248,6 +309,7 @@ int main(int argc, char *argv[]) {
     std::vector<std::string> paths;
     std::string case_input;
     bool rename_parents = false;
+    bool rename_only_extension = false; // Flag for -ce option
 
     bool case_specified = false;
 
@@ -286,6 +348,20 @@ int main(int argc, char *argv[]) {
                     print_error("\033[1;91mError: Missing argument for option -c\n");
                     return 1;
                 }
+            } else if (arg == "-ce") { // Handle -ce option
+                rename_only_extension = true;
+                if (i + 1 < argc) {
+                    case_input = argv[++i];
+                    case_specified = true;
+                    // Check if the case mode is valid
+                    if (case_input != "lower" && case_input != "upper" && case_input != "reverse" && case_input != "fupper") {
+                        print_error("\033[1;91mError: Unspecified case mode. Please specify 'lower', 'upper', 'reverse', or 'fupper'.\n");
+                        return 1;
+                    }
+                } else {
+                    print_error("\033[1;91mError: Missing argument for option -ce\n");
+                    return 1;
+                }
             } else {
                 paths.emplace_back(arg);
             }
@@ -293,7 +369,7 @@ int main(int argc, char *argv[]) {
     }
 
     if (!case_specified) {
-        print_error("\033[1;91mError: Case conversion mode not specified (-c or -cp option is required)\033[0m\n");
+        print_error("\033[1;91mError: Case conversion mode not specified (-c, -cp, or -ce option is required)\033[0m\n");
         return 1;
     }
 
@@ -332,14 +408,20 @@ int main(int argc, char *argv[]) {
     if (verbose_enabled) {
         std::cout << "\n";
     }
-    
-    rename_path(paths, case_input, rename_parents, verbose_enabled);
-    
+
+    // Determine whether to call rename_path or rename_only_extension based on the option specified
+    if (rename_only_extension) {
+        rename_path(paths, case_input, false, verbose_enabled, true);
+    } else {
+        rename_path(paths, case_input, rename_parents, verbose_enabled);
+    }
+
     std::cout << "\n";
-    
+
     std::cout << "\033[1mPress enter to exit...";
     std::cin.get();
     std::system("clear");
 
     return 0;
 }
+
