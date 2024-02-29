38ae734ffc63d734796170183c8f5f4131dd00d5
siyia2 <46220960+siyia2@users.noreply.github.com>
2024-02-29 16:01:14 +0200

    Update bulk_rename++.cpp

--- bulk_rename++.cpp
+++ bulk_rename++.cpp
@@ -814,7 +814,7 @@ int main(int argc, char *argv[]) {
     // Check if --version flag is present
     if (argc > 1 && std::string(argv[1]) == "--version") {
         // Print version number and exit
-        printVersionNumber("1.3.8");
+        printVersionNumber("1.3.9");
         return 0;
     }
 
