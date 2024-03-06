fb69afccb2827988e53b86b7582ba685c88fbf6f
siyia2 <46220960+siyia2@users.noreply.github.com>
2024-03-06 21:56:35 +0200

    Add files via upload

--- headers.h
+++ headers.h
@@ -28,6 +28,7 @@ extern bool special;
 
 // case modes
 
+// General
 std::string sentenceCase(const std::string& string);
 std::string to_pascal(const std::string& string);
 std::string from_pascal_case(const std::string& string);
@@ -41,6 +42,7 @@ std::string append_numbered_prefix(const std::filesystem::path& parent_path, con
 std::string remove_numbered_prefix(const std::string& file_string);
 std::string append_date_seq(const std::string& file_string);
 std::string remove_date_seq(const std::string& file_string);
+// Mv style for folder renaming only
 void remove_sequential_numbering_from_folders(const std::filesystem::path& base_directory, int& dirs_count, bool verbose_enabled, bool symlinks);
 void rename_folders_with_sequential_numbering(const std::filesystem::path& base_directory, std::string prefix, int& dirs_count, bool verbose_enabled, bool symlinks);
 void rename_folders_with_sequential_numbering(const std::filesystem::path& base_directory, int& dirs_count, bool verbose_enabled, bool symlinks);
@@ -49,15 +51,19 @@ void remove_date_suffix_from_folders(const fs::path& base_directory, int& dirs_c
 
 // main
 
+// General
 void print_error(const std::string& error, std::ostream& os = std::cerr);
 void print_verbose_enabled(const std::string& message, std::ostream& os = std::cout);
 void printVersionNumber(const std::string& version);
 void print_help();
+// For file extensions
 void batch_rename_extension(const std::vector<std::pair<fs::path, fs::path>>& data, bool verbose_enabled, int& files_count, bool symlinks);
 void rename_extension(const std::vector<fs::path>& item_paths, const std::string& case_input, bool verbose_enabled, int& files_count, size_t batch_size_files, bool symlinks);
 void rename_extension_path(const std::vector<std::string>& paths, const std::string& case_input, bool verbose_enabled, int depth, int& files_count, size_t batch_size_files, bool symlinks);
+// For file renaming
 void rename_file(const fs::path& item_path, const std::string& case_input, bool is_directory, bool verbose_enabled, bool transform_dirs, bool transform_files, int& files_count, int& dirs_count, size_t batch_size_files, bool symlinks);
 void rename_batch(const std::vector<std::pair<fs::path, std::string>>& data, bool verbose_enabled, int& files_count, int& dirs_count);
+// For folder renaming
 void process_forParents(const fs::directory_entry& entry, const std::string& case_input, bool verbose_enabled, bool transform_dirs, bool transform_files, int& files_count, int& dirs_count, int depth, size_t batch_size_files, size_t batch_size_folders, bool symlinks);
 void rename_directory(const fs::path& directory_path, const std::string& case_input, bool rename_parents, bool verbose_enabled, bool transform_dirs, bool transform_files, int& files_count, int& dirs_count, int depth, size_t batch_size_files, size_t batch_size_folders, bool symlinks);
 void rename_path(const std::vector<std::string>& paths, const std::string& case_input, bool rename_parents, bool verbose_enabled, bool transform_dirs, bool transform_files, int depth, int dirs_count, int files_count, size_t batch_size_files, size_t batch_size_folders, bool symlinks);
