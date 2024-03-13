0ae62f42811b9822de8e499ec408ec883f5fab25
siyia2 <46220960+siyia2@users.noreply.github.com>
2024-03-13 12:44:01 +0200

    Add files via upload

--- bulk_rename++.cpp
+++ bulk_rename++.cpp
@@ -871,7 +871,7 @@ void rename_path(const std::vector<std::string>& paths, const std::string& case_
               << dirs_count << " folder(s) \033[1m\033[0m\033[1m| Skipped: \033[1;93m" << skipped_file_count << " file(s) \033[0m\033[1m&& \033[1;93m" << skipped_folder_count << " folder(s) \033[0m\033[0m\033[1m| From: \033[1;95m" << paths.size() << " input path(s)"
               << "\n\n\033[0m\033[1mTime Elapsed: " << std::setprecision(1)
               << std::fixed << elapsed_seconds.count() << "\033[1m second(s)\n";
-	} else if (special && non_interactive) {
+	} else if (special && !non_interactive) {
 			  // Output summary of the renaming process
     std::cout << "\n\033[0m\033[1mRenamed: \033[1;92m" << files_count << " file(s) \033[0m\033[1m&& \033[1;94m"
               << dirs_count << " folder(s) \033[1m\033[0m\033[1m| Skipped: \033[1;93m" << skipped_file_count << " file(s) \033[0m\033[1m&& \033[1;93m" << skipped_folder_special_count << " folder(s) \033[0m\033[0m\033[1m| From: \033[1;95m" << paths.size() << " input path(s)"
