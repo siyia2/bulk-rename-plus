19cde49c43de033b825c48a614503aee7db3525a
siyia2 <46220960+siyia2@users.noreply.github.com>
2024-03-24 22:40:52 +0200

    Add files via upload
    Signed-off-by: siyia2 <46220960+siyia2@users.noreply.github.com>

--- bulk_rename++.cpp
+++ bulk_rename++.cpp
@@ -871,7 +871,7 @@ int main(int argc, char *argv[]) {
     // Check if --version flag is present
     if (argc > 1 && std::string(argv[1]) == "--version") {
         // Print version number and exit
-        printVersionNumber("1.8.1");
+        printVersionNumber("1.8.2");
         return 0;
     }
 
