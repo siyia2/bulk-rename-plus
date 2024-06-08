e3780b06ef6f18ec360949109ab4dab21b812eaf
siyia2 <46220960+siyia2@users.noreply.github.com>
2024-06-08 19:20:34 +0300

    version bump
    Signed-off-by: siyia2 <46220960+siyia2@users.noreply.github.com>

--- src/bulk_rename++.cpp
+++ src/bulk_rename++.cpp
@@ -896,7 +896,7 @@ int main(int argc, char *argv[]) {
     // Check if --version flag is present
     if (argc > 1 && std::string(argv[1]) == "--version") {
         // Print version number and exit
-        printVersionNumber("1.9.8");
+        printVersionNumber("1.9.9");
         return 0;
     }
 
